a46cfa93ea8c1530b9425fa249b9b11faea5af8c
marekspaleniak <marek.spaleniak@hotmail.com>
2024-04-18 14:00:15 +0200

    Update Sample.java
    Changed from Manisha onto Marek

--- Sample.java
+++ Sample.java
@@ -3,7 +3,7 @@ class Sample
     public static void main(String ar[])
     {
       System.out.println("Git load demo");
-      System.out.println("Hello from Manisha");
+      System.out.println("Hello from Marek");
       for(int i=0;i<4;i++)
         {
           System.out.println("Welcome "+i);
