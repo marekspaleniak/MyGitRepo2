3b173f5c17e865c5de32422dca58c38c5b04beff
Manisha-atos <62686138+Manisha-atos@users.noreply.github.com>
2024-04-19 15:33:13 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -4,10 +4,7 @@ class Sample
     {
       System.out.println("Git load demo");
       System.out.println("Hello from Manisha");
-      for(int i=0;i<4;i++)
-        {
-          System.out.println("Welcome "+i);
-        }
+     
 System.out.println("thank you ");
     }
   }
